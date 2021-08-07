4f69f6e35a9bc66b62265558405cc38d9d85ace2
Guanjun <haogj@hotmail.com>
2021-08-07 11:40:32 +0800

    add read me

--- day11_optimize/Symbols.cs
+++ day11_optimize/Symbols.cs
@@ -4,93 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace week2
-{
-    public class Symbols
-    {
+namespace week2 {
+    public class Symbols {
         public Symbols Outer { get; private set; }
         public IDictionary<string, int> Table { get; private set; }
         public int Size { get { return Table.Count; } }
-        public Symbols() {
-            Init(null); 
+        public Symbols () {
+            Init (null);
         }
-        public Symbols(Symbols outer)
-        {
-            Init(outer);
+        public Symbols (Symbols outer) {
+            Init (outer);
         }
 
-        private void Init(Symbols outer)
-        {
+        private void Init (Symbols outer) {
             Outer = outer;
-            this.Table = new Dictionary<string, int>();
+            this.Table = new Dictionary<string, int> ();
         }
 
-        public void Append(Symbols s)
-        {
-            foreach(var pair in s.Table)
-            {
-                Table.Append(pair);
+        public void Append (Symbols s) {
+            foreach (var pair in s.Table) {
+                Table.Append (pair);
             }
         }
-        public int Find(string key)
-        {
+        public int Find (string key) {
             return Table[key];
         }
 
-        public Location Get(string key)
-        {
-            return Get(key);
+        public Location Get (string key) {
+            return Get (key);
         }
 
-        public Location Get(string key, int nest)
-        {
+        public Location Get (string key, int nest) {
             int index = Table[key];
-            if( Table.ContainsKey(key))
-            {
-                return new Location(nest, index);
-            }
-            else
-            {
-                if(Outer == null)
-                {
+            if (Table.ContainsKey (key)) {
+                return new Location (nest, index);
+            } else {
+                if (Outer == null) {
                     return null;
+                } else {
+                    return Outer.Get (key, nest - 1);
                 }
-                else
-                {
-                    return Outer.Get(key, nest - 1);
-                }
-                
+
             }
         }
 
-        public int PutNew(string key)
-        {
-            int? i = Find(key);
-            if( i == null)
-            {
-                return Add(key);
-            }
-            else
-            {
+        public int PutNew (string key) {
+            int? i = Find (key);
+            if (i == null) {
+                return Add (key);
+            } else {
                 return i.Value;
             }
         }
 
-        public Location Put(string key)
-        {
-            Location loc = Get(key, 0);
-            if( loc == null)
-            {
-                return new Location(0, Add(key));
-            }
-            else
-            {
+        public Location Put (string key) {
+            Location loc = Get (key, 0);
+            if (loc == null) {
+                return new Location (0, Add (key));
+            } else {
                 return loc;
             }
         }
 
-        public int Add(string key)
-        {
+        public int Add (string key) {
             int count = Size;
             Table[key] = count;
             return count;
@@ -98,12 +74,13 @@ namespace week2
 
     }
 
-    public class Location
-    {
+    public class Location {
+        // 嵌套的层数，最内层为 0, 向外依此增加 1
         public int Nest { get; private set; }
+
+        // 变量在作用域所处的下标
         public int Index { get; private set; }
-        public Location(int nest, int index)
-        {
+        public Location (int nest, int index) {
             Nest = nest;
             Index = index;
         }
